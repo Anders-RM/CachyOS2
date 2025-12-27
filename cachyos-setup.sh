e6d40f1f28a4cff39a7c15e71c105c10b2131226
Anders-RM <usual.dusk6145@fastmail.com>
2025-12-27 23:12:12 +0100

    test2

--- cachyos-setup.sh
+++ cachyos-setup.sh
@@ -252,10 +252,10 @@ configure_reflector() {
 
     # Enable and start the reflector timer and service
     print_status "Enabling and starting reflector timer..."
-    #systemctl enable --now reflector.timer
+    systemctl enable --now reflector.timer
 
     print_status "Enabling and starting reflector service..."
-    #systemctl enable --now reflector.service
+    systemctl enable --now reflector.service
 
     # Modify the reflector.timer configuration to run daily at 18:00
     print_status "Setting reflector timer to run daily at 18:00..."
@@ -731,7 +731,7 @@ main() {
     install_cider
     install_aur_packages
     configure_filen
-    configure_reflector
+    #configure_reflector
     configure_flatpak
     configure_desktop_trashcan
     configure_cosmic_desktop
